e8a9a54a1d3ed1da71a452751b6cf4b982a197a1
dohertre <dohertre@rose-hulman.edu>
2019-06-19 17:12:14 -0400

    random

--- src/m1r_mutation_vs_reassignment.py
+++ src/m1r_mutation_vs_reassignment.py
@@ -4,7 +4,7 @@ This module demonstrates the distinction betweeen:
   -- RE-ASSIGNING the name BLAH to refer to a different object.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Rebekah Doherty.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
